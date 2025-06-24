0cbca17e2b3e5282772ee1327e4377109c782cf6
pagal <pagalavanks.22cse@kongu.edu>
2025-06-24 22:39:21 +0530

    d.5

--- calendar-web/vite.config.js
+++ calendar-web/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 // vite.config.js
-export default {
-  base: './',
-};
+export default defineConfig({
+  plugins: [react()],
+})
 
